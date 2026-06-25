2b5e5f03c46c81d90cbf487c0b95c46bcd6a4fd6
tianbao2381357587-del <tianbao2381357587@gmail.com>
2026-06-25 09:47:18 +0800

    Upload main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+﻿import os
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
+from adami_canvas_backend.server import app
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=3000, ws_ping_interval=None, ws_ping_timeout=None)
+
+
